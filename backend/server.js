f6b58ad97e912c3631e5885206e926f643dfa467
JustineBargas <104289374+JustineBargas@users.noreply.github.com>
2025-05-03 23:46:56 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -38,10 +38,10 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 const db = mysql.createConnection({
-  host: "mysql-1d6ccb68-justinebarias111-62a0.l.aivencloud.com",
+  host: HOST,
   port: 17290,
-  user: "USER",
-  password: "PASS",
+  user: USER,
+  password: PASS,
   database: "clean_up_tracker",
   ssl: {
     ca: fs.readFileSync(path.join(__dirname, 'cert', 'ca.pem')),  // Use path.join to reference the ca.pem file
